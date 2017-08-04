9c170fb5cfd25570288870b910c80b56139fc9a3
Olivia Mo <oliviamo@Olivias-MacBook-Pro.local>
2017-08-04 15:13:52 +1000

    first commit

---
+++ atm.rb
@@ -0,0 +1,20 @@
+=begin
+Create a terminal app that behaves like an ATM.
+includes:
+pin
+fees
+accounts
+language
+
+requirements:
+withdrawl
+check balance
+deposit
+greetings
+
+MVP = minimal viable product
+=end
+
+=begin
+greeting then method of menu. Include 'back' button
+=end
